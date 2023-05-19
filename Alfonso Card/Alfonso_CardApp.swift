0998d2c225638fa12c344b146d3113ea1fa9fefe
ponchotonic <ponchotonic@gmail.com>
2023-05-19 12:32:34 -0600

    Initial Commit

---
+++ Alfonso Card/Alfonso_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alfonso_CardApp.swift
+//  Alfonso Card
+//
+//  Created by Alfonso Castro Flores on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Alfonso_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
